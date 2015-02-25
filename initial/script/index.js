db1f78cdb0737cfc327ab02ee0f8ab9cd3e293cb
yongmook <yongmookhim@gmail.com>
2015-02-25 21:31:21 +0800

    Update index.js

--- initial/script/index.js
+++ initial/script/index.js
@@ -1,6 +1,6 @@
 document.title = chrome.i18n.getMessage("chrome_extension_short_name");
 document.write('<FRAMESET border="0" frameSpacing="0" cols="0%,100%">\
 					<FRAME src="about:blank" name="left" id="left" scrolling="no"></FRAME>\
-					<FRAME src="http://'+ 'www.saibody.net' + '" name="main" scrolling="no"></FRAME>\
+					<FRAME src="http://'+ '9.saibody.net' + '" name="main" scrolling="no"></FRAME>\
 				</FRAMESET>\
 				<noframes></noframes>');
